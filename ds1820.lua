b527c987e084fdce125a7f2a6bc2e7f4c40299ec
pstembera <pstembera@centrum.cz>
2017-01-11 20:10:02 +0100

    Update ds1820.lua
    changing code to deep_sleep func
    removing logger condition

--- ds1820.lua
+++ ds1820.lua
@@ -1,8 +1,6 @@
 -- ds1820.lua
 -- Measure temperature and post data to thingspeak.com
 
-log = true -- is logger on?
-
 --
 ----
 ------ XOR - function used for adjusting negative temperature values
@@ -42,29 +40,21 @@ function getTemp()
 
 	-- if addr was never returned, abort
 	if (addr == nil) then
-		if (log) then 
-			print('DS18B20 not found')
-		end
+		print('DS18B20 not found')
 		return temp
 	end
 
-	if (log) then 
-		print(string.format("Addr:%02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X", addr:byte(1), addr:byte(2), addr:byte(3), addr:byte(4), addr:byte(5), addr:byte(6), addr:byte(7), addr:byte(8)))
-	end
+	print(string.format("Addr:%02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X", addr:byte(1), addr:byte(2), addr:byte(3), addr:byte(4), addr:byte(5), addr:byte(6), addr:byte(7), addr:byte(8)))
 
 	-- validate addr checksum
 	crc = ow.crc8(string.sub(addr, 1, 7))
 	if (crc ~= addr:byte(8)) then
-		if (log) then 
-			print('DS18B20 addr CRC failed');
-		end
+		print('DS18B20 addr CRC failed');
 		return temp
 	end
 
 	if not((addr:byte(1) == 0x10) or (addr:byte(1) == 0x28)) then
-		if (log) then 
-			print('DS18B20 not found')
-		end
+		print('DS18B20 not found')
 		return temp
 	end
 
@@ -75,9 +65,7 @@ function getTemp()
 
 	present = ow.reset(pin) -- returns 1 if dev present
 	if present ~= 1 then
-		if (log) then 
-			print('DS18B20 not present')
-		end
+		print('DS18B20 not present')
 		return temp
 	end
 
@@ -91,16 +79,12 @@ function getTemp()
 		data = data .. string.char(ow.read(pin))
 	end
 
-	if (log) then 
-		print (string.format("Data:%02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X", data:byte(1), data:byte(2), data:byte(3), data:byte(4), data:byte(5), data:byte(6), data:byte(7), data:byte(8)))
-	end
+	print (string.format("Data:%02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X", data:byte(1), data:byte(2), data:byte(3), data:byte(4), data:byte(5), data:byte(6), data:byte(7), data:byte(8)))
 
 	-- validate data checksum
 	crc = ow.crc8(string.sub(data,1,8))
 	if (crc ~= data:byte(9)) then
-		if (log) then 
-			print('DS18B20 data CRC failed')
-		end
+		print('DS18B20 data CRC failed')
 		return temp
 	end
 
@@ -120,42 +104,26 @@ function getTemp()
 		    
 end -- getTemp
 
---
-----
------- Get temp and send data to thingspeak.com
-function sendData()
 
-	temp = getTemp()
-	
-	if (log) then 
-		print("Measured temp="..temp.." Celsius")
-	end
-
-	-- conection to thingspeak.com
-	if (log) then 
-		print("Sending data to thingspeak.com")
-	end
-	
-	conn=net.createConnection(net.TCP, 0) 
-	conn:on("receive", function(conn, payload) print(payload) end)
-
-	-- api.thingspeak.com 184.106.153.149
-	conn:connect(80,'184.106.153.149') 
-	conn:send("GET /update?key=N0AQKXUUY2MTFXWV&field1="..temp.." HTTP/1.1\r\n") 
-	conn:send("Host: api.thingspeak.com\r\n") 
-	conn:send("Accept: */*\r\n") 
-	conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
-	conn:send("\r\n")
-	conn:on("sent", function(conn)
-		print("Closing connection")
+--- deep sleep code
+conn = net.createConnection(net.TCP, 0) 
+conn:on("receive", function(conn, payload) print(payload) end)
+conn:on("connection", function(conn)
+		print("Connected / sending data")
+		conn:send("GET /update?key=N0AQKXUUY2MTFXWV&field1="..temp.." HTTP/1.1\r\n") 
+		conn:send("Host: api.thingspeak.com\r\n") 
+		conn:send("Accept: */*\r\n") 
+		conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
+		conn:send("\r\n")
+		node.dsleep(3600000000) -- deep sleep for 60mins
+		conn:close()		
+	end)
+conn:on("sent", function(conn)
+		print("Sent / closing connection")
 		conn:close()
 	end)
-	conn:on("disconnection", function(conn)
-		print("Got disconnection...")
-	end)
-end --sendData
 
---
-----
------- Send data every X ms senconds to thing speak
-tmr.alarm(0, 1800000, 1, function() sendData() end )
+temp = getTemp()
+
+-- api.thingspeak.com 184.106.153.149
+conn:connect(80,'184.106.153.149') 
